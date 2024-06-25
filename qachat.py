adf2a351c7569700d503d4508ca6320d997f46d5
Arpan Sethi <arpansethi30@gmail.com>
2024-06-25 17:38:27 +0530

    chatbot

--- qachat.py
+++ qachat.py
@@ -32,3 +32,15 @@ if "chat_history" not in st.session_state:
 
 input = st.text_input("Input:", key="input")
 submit = st.button("Ask the question")
+
+if submit and input:
+    response = get_gemini_response(input)
+    ## Add user query and response to chat history
+    st.session_state["chat_history"].append(("You", input))
+    st.subheader("Response:")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state["chat_history"].append(("Bot", chunk.text))
+st.subheader("Chat History:")
+for role, text in st.session_state["chat_history"]:
+    st.write(f"{role}: {text}")
